3c701c2ec3820bee8d942040b4627dd06214bd06
Christian Toledo <chrstiantoledo@gmail.com>
2022-01-29 15:45:35 -0300

    create boilerplate

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -9,4 +9,15 @@ describe('<Main/>', () => {
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
   })
+
+  it('should render colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      backgroundColor: '#06092b'
+    })
+  })
 })
